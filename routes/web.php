a913cf83e89ef73f2456c7f6ee0c1d2e9527d43a
topcoder <topcoder728@mail.ru>
2021-11-01 11:12:39 -0700

    FIX

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,7 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
 Route::fallback(function () {
     return view('layouts.app');
 });
